e5a2a65e2d08800d94acdf4e02adb0e4c8c59e89
xl <l.margaret.liu@gmail.com>
2024-03-28 17:35:11 +1100

    menu item state mgmt

--- client/src/components/ExploreMenu/ExploreMenu.jsx
+++ client/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = () => {
+  const [category, setCategory] = useState("All")
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
@@ -10,8 +12,8 @@ const ExploreMenu = () => {
       <div className='explore-menu-list'>
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt="" />
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
               <p>{item.menu_name}</p>
             </div>
           )
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import './Home.css'
 
+
 const Home = () => {
+
   return (
     <div>
       <Header />
